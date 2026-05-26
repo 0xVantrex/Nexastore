d21f87e75b2e56e9a2e31ea4708c4eae271f9b6b
0xvantrex <callagansikuche0@gmail.com>
2026-05-26 09:21:32 +0300

    add project unmodified

---
+++ app/Database/Migrations/2026-05-26-060245_CreateUsersTable.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 150,
+                'unique=' => true,
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => false,
+            ],
+            'role' => [
+                'type' => 'ENUM',
+                'constraint' => ['admin', 'editor', 'user'],
+                
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addprimarykey('id');
+        $this->forge->addUniqueKey('email');
+        $this->forge->createTable('users');
+
+    }
+
+    public function down()
+    {
+        $this-> forge->dropTable('users');
+        }
+}
