39240ffd1c6c43aaedba61b3b2992e46dde5c0ee
0xvantrex <callagansikuche0@gmail.com>
2026-05-26 09:41:10 +0300

    CreateProductsTable

--- app/Database/Migrations/2026-05-26-060258_CreateProductsTable.php
+++ app/Database/Migrations/2026-05-26-060258_CreateProductsTable.php
@@ -8,11 +8,57 @@ class CreateProductsTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'price' => [
+                'type' => 'DECIMAL',
+                'constraint' => '10,2',
+            ],
+            'stock' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'default' => 0,
+            ],
+            'image' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'created_by' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_by' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+            ]
+        ]);
+        $this->forge->addprimaryKey('id');
+        $this->forge->createTable('products');
+        $this->forge->addForeignKey('created_by', 'users','id', 'CASCADE');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('products');
     }
 }
