2dd798c4090867af477f73451d48dc7a4f064761
0xvantrex <callagansikuche0@gmail.com>
2026-05-26 11:54:05 +0300

    typo in routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -19,28 +19,28 @@ $routes->get('/register', 'Auth::register');
 $routes->post('/register', 'Auth::registerUser');
 $routes->get('/logout', 'Auth::logout');
 
-//Protected routes - all logged in users
-$routes->group('', ['filter' => 'auth'], function($routes) {
-    $routes->get('/dashboard', 'dashboard::index');
-    $routes->get('/profile', 'Dashboard::profile');
-    $routes->post('/my-orders', 'Dashboard::myOrders');
+// Protected routes - all logged in users
+$routes->group('', ['filter' => 'auth'], function(\CodeIgniter\Router\RouteCollection $routes) {
+    $routes->get('dashboard', 'Dashboard::index');
+    $routes->get('profile', 'Dashboard::profile');
+    $routes->get('my-orders', 'Dashboard::myOrders');
 });
 
-//Editor + admin routes
-$routes->group('', ['filter' => 'role:editor,admin'], function($routes){
-    routes->get('/products/create', 'Products::create');
-    routes->post('/products/create', 'Products::store');
-    routes->get('/products/edit/(:num)', 'Products::edit/$1');
-    routes->post('/products/edit/(:num)', 'Products::update/$1');
-    routes->post('/products/delete/(:num)', 'Products::delete/$1');
+// Editor + Admin routes
+$routes->group('', ['filter' => 'role:editor,admin'], function(\CodeIgniter\Router\RouteCollection $routes) {
+    $routes->get('products/create', 'Products::create');
+    $routes->post('products/create', 'Products::store');
+    $routes->get('products/edit/(:num)', 'Products::edit/$1');
+    $routes->post('products/edit/(:num)', 'Products::update/$1');
+    $routes->get('products/delete/(:num)', 'Products::delete/$1');
 });
 
-//Admin only routes
-$routes->group('', ['filter' => 'role:admin'], function($routes){
-    $routes->get('/admin', 'Admin::index');
-    $routes->get('/admin/users', 'Admin::users');
-    $routes->get('/admin/orders', 'Admin::orders');
-    $routes->post('/admin/orders/(:num)', 'Admin::updateStatus/$1');
-    $routes->post('/admin/orders/status/(:num)', 'Admin::updateStatus/$1');
-    $routes->post('/admin/users/(:num)/delete', 'Admin::deleteUser/$1');
+// Admin only routes
+$routes->group('', ['filter' => 'role:admin'], function(\CodeIgniter\Router\RouteCollection $routes) {
+    $routes->get('admin', 'Admin::index');
+    $routes->get('admin/users', 'Admin::users');
+    $routes->get('admin/orders', 'Admin::orders');
+    $routes->get('admin/orders/(:num)', 'Admin::orderDetail/$1');
+    $routes->post('admin/orders/status/(:num)', 'Admin::updateStatus/$1');
+    $routes->get('admin/users/delete/(:num)', 'Admin::deleteUser/$1');
 });
